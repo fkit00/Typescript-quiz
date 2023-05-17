ce512fae0dcd9d59cd566537b4e89b8d8dc324e2
fiona <fiona.kitchen@yahoo.co.uk>
2023-05-17 10:58:05 +0100

    working utils

--- src/API.ts
+++ src/API.ts
@@ -1,6 +1,12 @@
 
 
 export type Question ={
+category: string, 
+correct_answer: string,
+difficulty: string,
+incorrect_answer: string[],
+question: string, 
+type:string;
 
 }
 
@@ -10,9 +16,15 @@ export enum Difficulty{
     HARD="hard"
 }
 
+export type QuestionState = Question & { answers: string[]}
 
 export async function fetchQuizQuestions(amount:number, difficulty:Difficulty){
     const endpoint=`https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
     const data = await (await fetch(endpoint)).json()
     console.log(data)
+
+return data.results.map((question: Question)=>({
+    ...question, 
+    answer: 
+}))
 }
