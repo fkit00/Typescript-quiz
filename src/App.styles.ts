67044a530c24e39bd2c839d8c2c94577022e0f2b
fiona <fiona.kitchen@yahoo.co.uk>
2023-05-22 10:16:12 +0100

    working on styling

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,5 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components"
-import bg_image from "./images/bg-image.jpg"
+import bgImage from "./images/bgImage.jpg"
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -7,8 +7,8 @@ html{
     height:100%;
 }
 
-body{
-  
+body{   
+    background-image: url(${bgImage});
     background-size: cover;
     margin:0;
     padding: 0 20px;
@@ -19,29 +19,35 @@ body{
 
 *{
     box-sizing: border-box; 
-    font-family:"arial"
+    font-family:"arial";
     
 }
 
 `
 
 export const Wrapper = styled.div`
-display:flex;
-flex-direction: column:
-alighn-items: center;
+
+    display:flex;
+    flex-direction: column:
+    align-items: center;
 
 
-> p{
-    color:white;
+> p {
+    color: white;
 }
 
 .score{
-    color:white, 
+    color: black;
     font-size: 2rem;
-    margin:0;
+    margin: 0;
 }
 
 .h1{
+background-image: linear-gradient(180deg,#fff,#87f1ff  );
+background-size: 100%;
+background-clip: text;
+-webkit-bacground-clip: text; 
+
 
 }
 
